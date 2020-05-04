b7bb5635ce42fda957e3a035605bec0b112244ef
Stella <momo63639@egmail.com>
2020-05-04 10:10:33 +0800

    level 4 bcrypt(hash)

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
-const md5 = require('md5');
+const bcrypt = require('bcryptjs');
+const saltRounds = 10;
 
 const app = express();
 
@@ -38,9 +39,10 @@ app.get("/register",function(req,res){
 });
 
 app.post("/register",function(req,res){
+bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
   const newUser = new User ({
     email:req.body.username,
-    password:md5(req.body.password)
+    password:hash
   });
   newUser.save(function(err){
     if (!err) {
@@ -50,17 +52,21 @@ app.post("/register",function(req,res){
     }
   });
 });
+
+});
 app.post("/login",function(req,res){
   const username = req.body.username;
-  const password = md5(req.body.password);
+  const password = req.body.password;
   User.findOne({email:username},function(err,founduser){
     if (err) {
       console.log(err);
     } else {
       if(founduser){
-        if(founduser.password === password){
-          res.render("secrets");
-        }
+        bcrypt.compare(password, founduser.password, function(err, result) {
+          if(result === true){
+            res.render("secrets");
+          }
+        });
       }
     }
   });
